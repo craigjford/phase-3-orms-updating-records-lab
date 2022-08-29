0e17203615a0bed9738f370a01bad89bcc49da57
Craig Ford <craig_ford@icloud.com>
2022-08-29 18:19:53 -0400

    commit tree 1ab2aa96c5292af2a727863c9eecc4ac19a0a1ef

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,48 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
+  attr_accessor :name, :grade
+  # attr_reader :id
 
+  def initialize(id: nil, name:, grade: )
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.create_table
+    sql = <<-SQL
+       CREATE TABLE students (
+        student_id INTEGER PRIMARY KEY,
+        name VARCHAR(255),
+        grade VARCHAR(255)
+       )
+    SQL
+    DB[:conn].execute(sql)
+
+  end
+
+  def self.drop_table
+    sql = <<-SQL 
+    DROP TABLE students 
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL 
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]   
+  end
+
+  def self.create_table
+    sql = <<-SQL 
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql)   
+  end
 end
